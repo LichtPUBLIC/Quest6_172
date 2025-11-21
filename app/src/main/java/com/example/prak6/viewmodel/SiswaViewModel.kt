43bde6d4d5dafac39435f730b506d2f56cc10a61
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-21 14:55:27 +0700

    Inisialisasi SiswaViewModel dan definisi MutableStateFlow

---
+++ app/src/main/java/com/example/prak6/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.prak6.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.prak6.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+}
