d5f1d85cb8ea0f707d416809853204dffdef08d0
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-21 14:53:10 +0700

    Menambahkan rute halaman Detail dan logika navigasi BackStack

--- app/src/main/java/com/example/prak6/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/prak6/view/uicontroller/Navigasi.kt
@@ -14,32 +14,33 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.prak6.model.DataJK.JenisK
 import com.example.prak6.view.FormIsian
+import com.example.prak6.view.TampilData
 import com.example.prak6.viewmodel.SiswaViewModel
 
 enum class Navigasi {
     Formulirku,
     Detail
 }
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaApp(
-    modifier: Modifier = Modifier,
+    modifier: Modifier,
     viewModel: SiswaViewModel = viewModel(),
-    navController: NavHostController = rememberNavController()
-) {
+    navController: NavHostController = rememberNavController(),
+
+    ) {
     Scaffold { isiRuang ->
         val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
-            modifier = Modifier.padding(isiRuang)
-        ) {
+
+            modifier = Modifier.padding(isiRuang)){
             composable(route = Navigasi.Formulirku.name) {
                 val konteks = LocalContext.current
                 FormIsian(
-                    pilihanJk = JenisK.map { id ->
-                        konteks.resources.getString(id)
+                    pilihanJk = JenisK.map { id -> konteks
+                        .resources.getString(id)
                     },
                     OnSubmitBtnClick = {
                         viewModel.setSiswa(it)
@@ -47,6 +48,20 @@ fun SiswaApp(
                     }
                 )
             }
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    statusUiSiswa = uiState.value,
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
         }
     }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Formulirku.name, inclusive = false)
 }
