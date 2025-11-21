c52903ae3f873a2b05fd6da2b14ecd1c16de94f2
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-21 14:42:59 +0700

    Membuat kerangka screen TampilData dengan Scaffold dan TopAppBar

---
+++ app/src/main/java/com/example/prak6/view/TampilSiswa.kt
@@ -0,0 +1,36 @@
+package com.example.prak6.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.prak6.R
+import com.example.prak6.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    statusUiSiswa: Siswa,
+    onBackBtnClick:()->Unit
+){
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id= R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id= R.color.purple_500))
+            )
+        }){ isiRuang ->
+
+        Text(
+            text = "Data akan muncul di sini",
+            modifier = Modifier.padding(isiRuang)
+        )
+    }
+}
