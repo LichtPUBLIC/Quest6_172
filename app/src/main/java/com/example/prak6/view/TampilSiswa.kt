66b727c843e6daf04df85642079e88fbb0f386e4
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-21 14:47:25 +0700

    Implementasi UI loop untuk menampilkan detail data siswa dengan custom font

--- app/src/main/java/com/example/prak6/view/TampilSiswa.kt
+++ app/src/main/java/com/example/prak6/view/TampilSiswa.kt
@@ -1,7 +1,10 @@
 package com.example.prak6.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -10,7 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.prak6.R
 import com.example.prak6.model.Siswa
 
@@ -33,10 +41,26 @@ fun TampilData(
                 colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id= R.color.purple_500))
             )
         }){ isiRuang ->
-
-        Text(
-            text = "Data akan muncul di sini",
-            modifier = Modifier.padding(isiRuang)
-        )
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            Column(
+                modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+            ) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(
+                            text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive,
+                            fontSize = 22.sp
+                        )
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+            }
+        }
     }
 }
